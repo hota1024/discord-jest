f6726bb74d05dc079ca3513f9d99757c12eeffd8
hota1024 <hotalog@hota1024.com>
2020-02-08 16:33:41 +0900

    :tada: Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverage: true
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Hello.
+ *
+ * @param name
+ */
+export const hello = (name: string): string => `Hello ${name}`
